390957b839dff9085225a60f2ae20219a2dfb6ed
42decibel <hyukzuny@gmail.com>
2024-05-10 21:04:30 +0900

    feat: Add default set

--- ic_env.py
+++ ic_env.py
@@ -1,6 +1,11 @@
 import json
 
-def settings():
+def open_settings():
     with open('./config/ic_settings.json', 'r') as json_file:
+        iset = json.load(json_file)
+    return iset
+
+def open_ic_default():
+    with open('./config/ic-preset/ic_settings.json', 'r') as json_file:
         iset = json.load(json_file)
     return iset

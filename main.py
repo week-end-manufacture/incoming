8839e6629efb29898e814acd1a769fb1bb954cf1
42decibel <hyukzuny@gmail.com>
2024-05-11 16:14:28 +0900

    feat: Add same logic

--- main.py
+++ main.py
@@ -30,9 +30,38 @@ def main():
         if (args.user_ext != None):
             print(args.user_ext)
     elif (args.user_preset != None):
+        print("!!!USER PRESET USE!!!")
+        print("SRC_DIR_PATH:[%s]" % src_dir_path)
+        print("DST_DIR_PATH:[%s]" % dst_dir_path)
+
         ic_preset = pre_processiong.open_ic_preset(args.user_preset)
+
+        filtered_video_ext_dict = ic_preset["filterd_all_ext_dict"]["filtered_video_ext_dict"]
+        filtered_image_ext_dict = ic_preset["filterd_all_ext_dict"]["filtered_image_ext_dict"]
+        filtered_archive_ext_dict = ["zip", "rar", "7z"]
+
+        print("filtered_video_ext_dict", filtered_video_ext_dict)
+
+        if (src_dir_path == dst_dir_path):
+            print("!!!SAME PATH PROCEDURE ACTIVATE!!!")
+
+        else:
+            print("!!!COPY CAT!!!")
+
+            src_icfilelist = pre_processiong.ic_serach(src_dir_path,
+                                                     filtered_video_ext_dict,
+                                                     filtered_image_ext_dict,
+                                                     filtered_archive_ext_dict)
+
+            print(src_icfilelist)
+
+            print("FILE LENGTH: %d" % len(src_icfilelist))
+
+            pre_processiong.print_video_icfile(src_icfilelist)
+            pre_processiong.print_image_icfile(src_icfilelist)
+            pre_processiong.print_archive_icfile(src_icfilelist)
     else:
-        print("!!!DEFAULT USE!!!")
+        print("!!!DEFAULT PRESET USE!!!")
         print("SRC_DIR_PATH:[%s]" % src_dir_path)
         print("DST_DIR_PATH:[%s]" % dst_dir_path)
 

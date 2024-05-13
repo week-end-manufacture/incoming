437089561f5b511252ec9726584301e957991413
LEE JUN HO <91662691+leejunho-com@users.noreply.github.com>
2024-05-13 17:06:25 +0900

    Adding options.

--- ic_image.py
+++ ic_image.py
@@ -1,8 +1,39 @@
-import sys
-from PIL import Image, ImageCms, ExifTags
+from PIL import Image, ImageCms
 
-import ic_process
+class ImageProcessor:
+    def __init__(self, image_path):
+        self.image = Image.open(image_path)
 
+    def image_process(self):
+        if self.preset['image_process_toggle']:
+            self.assign_untagged_icc_profile_to_sRGB()
+            self.remove_only_gps_exif_data()
+            self.remove_all_exif_data()
+            self.save_processed_image()
+
+    def assign_untagged_icc_profile_to_sRGB(self):
+        if self.preset['assign_untagged_icc_profile_to_sRGB']:
+            srgb_profile = ImageCms.createProfile("sRGB")
+            self.image = ImageCms.profileToProfile(self.image, srgb_profile, outputMode='RGB')
+
+    def remove_only_gps_exif_data(self):
+        if self.preset['remove_only_gps_exif_data']:
+            if 'exif' in self.image.info:
+                exif_dict = Image._getexif(self.image)
+                if 34853 in exif_dict:  # "34853" is a pointer to the GPS information IFD
+                    del exif_dict[34853]
+                self.image.info['exif'] = Image._makeexif(exif_dict)
+
+    def remove_all_exif_data(self):
+        if self.preset['remove_all_exif_data']:
+            self.image.info.pop('exif', None)
+
+    def save_processed_image(self):
+        output_ext = self.preset['output_ext']
+        output_quality = self.preset['output_quality']
+        self.image.save(f"processed_image.{output_ext}", quality=output_quality)
+
+'''
 class ImageProcessor:
     def __init__(self, ic_file, iset):
         self.image_path = ic_file.abs_path
@@ -40,10 +71,10 @@ class ImageProcessor:
         icc_profile = self.get_icc_profile()
         self.img.save(compressed_image_path, 'JPEG', quality=85, icc_profile=icc_profile)
 
-iset = ic_process.open_ic_preset()
 
 if __name__ == "__main__":
     processor = ImageProcessor(sys.argv[1])
     processor.remove_all_exif()  # 모든 EXIF 데이터 제거
     processor.remove_gps_info()  # GPS 정보만 제거
     processor.save_compressed_image("compressed_image.jpg")  # 압축된 이미지 저장
+'''
--- ic_preprocessing.py
+++ ic_preprocessing.py
@@ -25,13 +25,13 @@ class PreProcessing:
 
     def open_ic_default_preset(self):
         with open('./config/ic-preset/ic_preset.json', 'r') as json_file:
-            idps = json.load(json_file)
-        return idps
+            ipre = json.load(json_file)
+        return ipre
     
     def open_ic_user_preset(self, user_preset):
         with open('./config/ic-preset/' + user_preset + '.json', 'r') as json_file:
-            iups = json.load(json_file)
-        return iups
+            ipre = json.load(json_file)
+        return ipre
         
     def extract_if_contains_images(self,
                                    abs_path,

f5e11214c0beff77cc1b3b5d6a7179a8306297e9
42decibel <hyukzuny@gmail.com>
2025-05-27 09:46:09 +0900

    feat: Add ic rsc

--- src/deamon/main.c
+++ src/deamon/main.c
@@ -1,10 +1,23 @@
 #include "extern.h"
 
 
+void prog_usage()
+{
+    printf("Usage: incoming [options]\n");
+    printf("Options:\n");
+    printf("  -h, --help       Show this help message and exit\n");
+    printf("  -v, --version    Show version information and exit\n");
+}
+
 int main()
 {
     print_filelib();
-    printf("Hello, World!\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        sleep(1);
+        printf("Hello, World! %d\n", i);
+    }
 
     return (0);
 }
